86d13e7d0f442a367414f7fc0692fa93b10a6e89
Владимир Червяков <vladiiimir1991@gmail.com>
2022-09-11 13:36:15 +0300

    Добавьте файлы проекта.

---
+++ Module16.Practices/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module16.Practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Module16.PracticesTests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using Module16.Practices;
+using NUnit.Framework;
+
+namespace Module16.PracticesTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(7, 2) == 9);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(10, 2) == 8);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(3, 2) == 6);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(10, 5) == 2);
+        }
+
+        [Test]
+        public void DivisionMustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(4, 0));
+        }
+    }
+}
